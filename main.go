8cfa89f788d51139680f64377bac97eded1da021
郭书勤 <my943813636@gmail.com>
2024-01-10 03:06:36 +0800

    1

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	var port = flag.Int("port", 8278, "Port to listen on")
 	flag.Parse()
 	server, _ := socks5.New(&socks5.Config{})
-	if err := server.ListenAndServe("tcp", fmt.Sprintf("127.0.0.1:%d", *port)); err != nil {
+	if err := server.ListenAndServe("tcp", fmt.Sprintf("0.0.0.0:%d", *port)); err != nil {
 		log.Fatal(err)
 	}
 }
